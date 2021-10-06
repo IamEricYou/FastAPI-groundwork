b6679443cc11a3be10a98e402ab2531e85240444
Jaehyung You <jaehyung.you@travelflan.com>
2021-10-06 16:58:59 +0900

    UPDATE: error handler in global scope

--- app/main.py
+++ app/main.py
@@ -1,9 +1,25 @@
 from typing import Optional
-
 from fastapi import FastAPI
+from enum import Enum
+
+from starlette.requests import Request
+from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
 
+async def catch_exceptions_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception as e:
+        print(e)
+        return JSONResponse({"status": str(e)}, status_code=500)
+
+app.middleware('http')(catch_exceptions_middleware)
+
+class EnumClass(str, Enum):
+    a = "a"
+    b = "b"
+    c = "c"
 
 @app.get("/")
 async def read_root():
@@ -13,3 +29,7 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/enum/{enum_name}")
+async def read_enum(enum_name: EnumClass):
+    return EnumClass.value
